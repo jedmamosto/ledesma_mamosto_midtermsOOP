a1bdbbbd3c6afe67cefb07be6cb8739de61d7c4f
bit-Matt <124230426+bit-Matt@users.noreply.github.com>
2023-04-23 02:25:58 +0800

    Add methods and properties to child classes

--- classes/Bottoms.ts
+++ classes/Bottoms.ts
@@ -3,4 +3,46 @@ import { Clothing } from "./Clothing";
 
 export class Bottoms extends Clothing {
 
+  protected _isRipped: boolean;
+  protected _isLongPants: boolean;
+
+  constructor(
+    productName: string,
+    stockNumber: number,
+    retailPrice: number,
+    capitalCost: number,
+    isAvailable: boolean,
+    isOnSale: boolean,
+    size: string,
+    color: string,
+    material: string,
+    brand: string,
+    isRipped: boolean,
+    isLongPants: boolean
+  ) {
+    super(
+      productName, 
+      stockNumber, 
+      retailPrice, 
+      capitalCost, 
+      isAvailable, 
+      isOnSale, 
+      size, 
+      color, 
+      material, 
+      brand
+    );
+
+    this._isRipped = isRipped;
+    this._isLongPants = isLongPants;
+  }
+
+  public hemPants(inchLength: number): string {
+    if (this._isLongPants === true) {
+      return `These pants will be hemmed by ${inchLength} inches.`
+    }
+
+    return `Invalid. Product is not categorized as Long Pants.`
+  }
+
 }
