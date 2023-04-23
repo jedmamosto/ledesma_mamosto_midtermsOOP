d2b047a32054acd4f55e7301a596d3cd46decad3
bit-Matt <124230426+bit-Matt@users.noreply.github.com>
2023-04-23 12:09:35 +0800

    Add methods and test cases
    Add and updates more methods to classes and implemented test cases to main

--- classes/Bottoms.ts
+++ classes/Bottoms.ts
@@ -37,12 +37,49 @@ export class Bottoms extends Clothing {
     this._isLongPants = isLongPants;
   }
 
+  // Unique method:
   public hemPants(inchLength: number): string {
     if (this._isLongPants === true) {
       return `These pants will be hemmed by ${inchLength} inches.`
     }
 
     return `Invalid. Product is not categorized as Long Pants.`
+  }
+
+  //Unique implemented methods:
+  public browseSimilarProducts(): string {
+    return `Here are products similar to ${this._productName} bottom.`
+  }
+
+  public notifyShip(): string {
+    return `${this._productName} bottom has been shipped.`
+  }
+
+  public notifyRestock(): string {
+    return `${this._productName} bottom has been restocked.`
+  }
+
+  public writeReview(): string {
+    return 'User has written a review about this bottom.'
+  }
+
+  public getDescription(): string {
+    let pantLength: string = '';
+    let ripped: string = '';
+
+    if (this._isLongPants === true) {
+      pantLength = 'long pants';
+    } else {
+      pantLength = 'shorts';
+    }
+
+    if (this._isRipped === true) {
+      ripped = 'ripped';
+    } else {
+      ripped = 'non-ripped';
+    }
+
+    return `${this._productName} ${ripped} ${pantLength} by ${this._brand} of size ${this._size} has a color of ${this._color}, is made of ${this._material} material, and costs P${this._retailPrice}.`;
   }
 
 }
--- interfaces/OnlineShopping.ts
+++ interfaces/OnlineShopping.ts
@@ -2,7 +2,7 @@ export interface OnlineShopping {
   createAccount(): string; // ask for userName and userPassword returns message
   login(): string; // let user input userName and userPassword returns message
   logout(): string; // ends program
-  browseProducts(): string; // show user list of products
+  browseSimilarProducts(): string; // show user list of products
   notifyShip(): string; // returns message
   notifyRestock(): string; // refills the stock
   getDiscountPrice(discount: number): number; // returns discounted price
